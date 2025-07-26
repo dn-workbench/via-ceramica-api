ac86d6f2750d1d3351ca2ce83b143763ac278088
Daniil <daniil.narozhnyy@gmail.com>
2025-07-26 21:01:09 +0300

    Add products endpoint

--- main.py
+++ main.py
@@ -1,14 +1,28 @@
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
 import json
 import os
 from parser import parse_xml
 
 app = FastAPI()
 
+# Разрешённые источники (фронтенд домен)
+origins = [
+    "https://via-ceramica-frontend.vercel.app",
+    # если хочешь, можешь добавить http://localhost:3000 для локальной разработки
+]
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,  # Можно указать ["*"] для всех, но это менее безопасно
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+
 @app.get("/products")
 def get_products():
     if not os.path.exists("data.json"):
-        # Первый запуск — сразу парсим и создаём файл
         try:
             parse_xml()
         except Exception as e:
